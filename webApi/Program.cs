2e072809f5424db1a526d38ac8dedd51be301589
TheNuRii <maciekglow0@gmail.com>
2024-03-05 23:14:34 +0100

    create Get method to my toDos

--- webApi/Program.cs
+++ webApi/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
 var todos = new List<Todo>();
 
-app.Run();
+app.MapGet("/todos", () => todos);
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null
+        ? TypedResults.NotFound()
+        : TypedResults.Ok(targetTodo);
+});
+
 
 app.MapPost("/todos", (Todo task) =>
 {
     todos.Add(task);
     return TypedResults.Created("/todos/{id}", task);
 });
+
+app.Run();
 
 public record Todo(int Id, string Name, DateTime DueDate, bool IsComplited);
