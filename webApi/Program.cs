f7e973ed741631fe4948cbe6ff194a76d8ccaee7
TheNuRii <maciekglow0@gmail.com>
2024-03-05 22:53:19 +0100

    Create toDo record and Post method

--- webApi/Program.cs
+++ webApi/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var todos = new List<Todo>();
 
 app.Run();
+
+app.MapPost("/todos", (Todo task) =>
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsComplited);
