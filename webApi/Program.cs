51eea0938eceb9091899b082ac876cab5633449e
TheNuRii <maciekglow0@gmail.com>
2024-03-06 23:43:11 +0100

    Create midlewear to write info aboute http requests

--- webApi/Program.cs
+++ webApi/Program.cs
@@ -1,7 +1,16 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("task/(.*)", "todos/"));
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
+});
 
 
 var todos = new List<Todo>();
