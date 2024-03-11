a583c5c910dc5318556879a59f06bb6d700e0e9f
TheNuRii <maciekglow0@gmail.com>
2024-03-11 19:53:44 +0100

    Creting first model caled (Product) and install necessary NuGet packages

---
+++ EntityFrameworkCore_basic/Models/Product.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoPizza.Models
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        
+        [Column(TypeName = "decimal(6, 2)")]
+        
+        public decimal Price { get; set; }
+    }
+}
