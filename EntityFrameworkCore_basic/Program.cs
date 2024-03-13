36056499c8bc71f3cbc9eee20949a860b230d449
TheNuRii <maciekglow0@gmail.com>
2024-03-13 23:12:28 +0100

    crate new 2 products in Proctuct

--- EntityFrameworkCore_basic/Program.cs
+++ EntityFrameworkCore_basic/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContex contex = new ContosoPizzaContex();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+contex.Products.Add(veggieSpecial);
+
+Product deluxMeat = new Product()
+{
+    Name = "Delux Max Pizza",
+    Price = 12.99M
+};
+contex.Products.Add(deluxMeat);
+
+contex.SaveChanges();
