a18b974eafe53ff1b524f0acb6445cd32fafd2be
TheNuRii <maciekglow0@gmail.com>
2024-03-19 22:45:15 +0100

    model builder for PokemonCategory and PokemonOwner

--- pokemon_card_api/Data/DataContex.cs
+++ pokemon_card_api/Data/DataContex.cs
@@ -18,5 +18,31 @@ public class DataContex : DbContext
     public DbSet<PokemonOwner> PokemonOwners { get; set; }
     public DbSet<Rewiew> Rewiews { get; set; }
     public DbSet<Rewiewer> Rewiewers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PokemonCategory>()
+            .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+        modelBuilder.Entity<PokemonCategory>()
+            .HasOne(p => p.Pokemon)
+            .WithMany(pc => pc.PokemonCategories)
+            .HasForeignKey(c => c.PokemonId);
+        modelBuilder.Entity<PokemonCategory>()
+            .HasOne(p => p.Category)
+            .WithMany(pc => pc.PokemonCategories)
+            .HasForeignKey(c => c.CategoryId);
+        
+        modelBuilder.Entity<PokemonOwner>()
+            .HasKey(pc => new { po.PokemonId, po.OwnerId });
+        modelBuilder.Entity<PokemonOwner>()
+            .HasOne(p => p.Pokemon)
+            .WithMany(pc => pc.PokemonOwners)
+            .HasForeignKey(c => c.PokemonId);
+        modelBuilder.Entity<PokemonOwner>()
+            .HasOne(p => p.Owner)
+            .WithMany(pc => pc.PokemonOwners)
+            .HasForeignKey(c => c.OwnerId);
+
+    }
     
 }
