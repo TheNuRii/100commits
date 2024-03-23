b5100ca4da586dd4741f41d49314d4aae86d8ccf
TheNuRii <maciekglow0@gmail.com>
2024-03-23 19:18:38 +0100

    Init Repository and Interface

---
+++ pokemon_card_api/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+using pokemon_card_api.Data;
+using pokemon_card_api.Model;
+
+namespace pokemon_card_api.Repository;
+
+public class PokemonRepository
+{
+    private readonly DataContext _context;
+    
+    public PokemonRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Pokemon> GetPokemons()
+    {
+        return _context.Pokemons.OrderBy(p => p.Id).ToList();
+    }
+}
+
