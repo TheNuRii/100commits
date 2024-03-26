a55848d60c345188ea1f602c9074629a8c9a9334
TheNuRii <maciekglow0@gmail.com>
2024-03-26 11:19:35 +0100

    Get request for pokemon rating with Dto

--- pokemon_card_api/Controllers/PokemonController.cs
+++ pokemon_card_api/Controllers/PokemonController.cs
@@ -29,6 +29,38 @@ public class PokemonController : Controller
 
         return Ok(pokemons);
     }
+
+    [HttpGet("{pokeId}")]
+    [ProducesResponseType(200, Type = typeof(Pokemon))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemon(int pokeId)
+    {
+        if (!_pokemonRepository.PokemonExists(pokeId))
+            return NotFound();
+
+        var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(pokemon);
+    }
+
+    [HttpGet("{pokeId/rating}")]
+    [ProducesResponseType(200, Type = typeof(decimal))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemonRating(int pokeId)
+    {
+        if (!_pokemonRepository.PokemonExists(pokeId))
+            return NotFound();
+
+        var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        return Ok(rating);
+    }
 }
 
 
